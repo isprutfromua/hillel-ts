0e6fdf8b3d850ad393083d66fa82689b2108fc34
Ivan Sprut <i.sprutfromua@gmail.com>
2023-10-28 23:09:38 +0300

    TS-002:ADD Implemented Area class

---
+++ src/lessons/02/Area.ts
@@ -0,0 +1,32 @@
+import { Level } from '@/lessons/02'
+import { ClassBuilder } from '@/helpers'
+
+type StaticFields = {
+  name: string
+}
+type EditableFieldsList = {
+  level: Level
+}
+type IArea = ClassBuilder<EditableFieldsList> & Readonly<StaticFields>
+
+export class Area implements IArea {
+  private _levels: Level[] = []
+
+  constructor(private readonly _name: string) {}
+
+  get name(): string {
+    return this._name
+  }
+
+  get levels() {
+    return this._levels
+  }
+
+  addLevel(level: Level) {
+    this._levels.push(level)
+  }
+
+  removeLevel(level: Level) {
+    this._levels = this._levels.filter(l => l !== level)
+  }
+}
