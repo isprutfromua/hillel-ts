f4c60153482ff6bbd0062540f5d96b8593f57a22
Ivan Sprut <i.sprutfromua@gmail.com>
2023-11-08 12:21:36 +0200

    TS-000:FIX Unnecessary data cleanup + README update

--- src/types/lesson04/index.ts
+++ src/types/lesson04/index.ts
@@ -1,29 +1,3 @@
-// In case of success
-// {
-// 	"status": "success",
-// 	"data": {
-// 		"databaseId": 567,
-// 		"sum": 10000,
-// 		"from": 2,
-// 		"to": 4
-// 	}
-// },
-
-// In case of fail
-// {
-// 	"status": "failed",
-// 	"data": {
-// 		"errorMessage": "....",
-// 		"errorCode": 4
-// 	}
-// }
-
-// {
-// 	"sum": 10000,
-// 	"from": 2,
-// 	"to": 4
-// }
-
 export interface PaymentRequest {
   sum: number
   from: number
@@ -45,22 +19,3 @@ export interface SuccessResponse {
   data: SuccessData
 }
 export type Response = FailedResponse | SuccessResponse
-
-// fields test
-const res: Response = {} as Response
-
-if (res.status === 'failed') {
-  console.log(res.data.errorCode)
-  console.log(res.data.errorMessage)
-  console.log(res.data.sum) //Error as expected
-} else if (res.status === 'success') {
-  console.log(res.data.databaseId)
-  console.log(res.data.from)
-  console.log(res.data.sum)
-  console.log(res.data.to)
-  console.log(res.data.errorCode) // Error as expected
-} else {
-  const invalidStatus: never = res
-
-  throw `invalid status ${invalidStatus}`
-}
