07cab9d042bf9b320ed2c13ed8bc37a7414764a8
Ivan Sprut <i.sprutfromua@gmail.com>
2023-12-02 14:42:12 +0200

    TS-008:ADD #8 Home work

--- src/lessons/08/deep-readonly.ts
+++ src/lessons/08/deep-readonly.ts
@@ -11,13 +11,13 @@ type FlatReadonly<T> = {
   readonly [key in keyof T]: Readonly<T[key]>
 }
 
-type DeepReadonlyX = DeepReadonly<TType>
-type DeepReadonlyG = DeepReadonlyX['g'] // readonly
+type DeepReadonlyType = DeepReadonly<TType>
+type DeepReadonlyG = DeepReadonlyType['g'] // readonly
 
 export type DeepReadonlyGG3 = DeepReadonlyG['g3'] // readonly
 export type DeepReadonlyGG333 = DeepReadonlyGG3['g33'] // readonly
 
-type FlatReadonlyX = FlatReadonly<TType>
-type FlatReadonlyG = FlatReadonlyX['g'] // readonly
+type FlatReadonlyType = FlatReadonly<TType>
+type FlatReadonlyG = FlatReadonlyType['g'] // readonly
 
 export type FlatReadonlyG3 = FlatReadonlyG['g3'] // non readonly
