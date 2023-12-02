e77ef4bc4bfb478e49d1265ca689aa75127f729e
Ivan Sprut <i.sprutfromua@gmail.com>
2023-12-02 14:41:32 +0200

    TS-008:ADD #8 Home work

--- src/lessons/08/deep-required-readonly.ts
+++ src/lessons/08/deep-required-readonly.ts
@@ -11,14 +11,14 @@ type FlatRequiredReadonly<T> = {
   readonly [key in keyof T]-?: Readonly<T[key]>
 }
 
-type DeepReadonlyX = DeepRequiredReadonly<TType>
-type DeepReadonlyG = DeepReadonlyX['g']
+type DeepReadonlyType = DeepRequiredReadonly<TType>
+type DeepReadonlyG = DeepReadonlyType['g']
 
 type DeepReadonlyGG3 = DeepReadonlyG['g3'] // readonly & required
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type DeepReadonlyGG3G33 = DeepReadonlyGG3['g33'] // readonly & required
 
-type FlatReadonlyX = FlatRequiredReadonly<TType>
-type FlatReadonlyG = FlatReadonlyX['g'] // readonly
+type FlatReadonlyType = FlatRequiredReadonly<TType>
+type FlatReadonlyG = FlatReadonlyType['g'] // readonly
 
 export type FlatReadonlyGG3 = FlatReadonlyG['g3'] // non readonly & non required
