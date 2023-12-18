809314dd5521f5ab4af38a34147feb7c654be501
Ivan Sprut <i.sprutfromua@gmail.com>
2023-12-18 22:44:43 +0200

    TS-010:ADD: Catch Decorator

---
+++ src/lessons/10/index.ts
@@ -0,0 +1,63 @@
+/*
+ * Написати декоратор методу @Catch, який має перехоплювати помилки та
+ * виводити повідомлення "Oops, there is an error in ${METHOD_NAME}: ${ERROR}".
+ *
+ * Наприклад
+ * class UsersService {
+ *   @Catch
+ *   getUsers() {
+ *     throw new Error("No users");
+ *   }
+ * }
+ *
+ * На виклику методу getUsers у консоль має вивестись повідомлення
+ * "Oops, there is an error in getUsers: No users"
+ * */
+
+const Catch = <T, A extends unknown[], R>(
+  originalMethod: (...args: unknown[]) => R,
+  context: ClassMethodDecoratorContext<T, (...args: A) => R>,
+) => {
+  if (context.kind !== 'method')
+    throw new Error(`Please use it for method only`)
+
+  function catcher(this: T, ...args: A): R | void {
+    try {
+      return originalMethod.apply(this, args)
+    } catch (e) {
+      if (e instanceof RangeError) {
+        console.error(
+          `Oops, there is a range error in ${String(context.name)}: ${
+            e.message
+          }`,
+        )
+      } else {
+        console.error(
+          `Oops, there is unrecognized error in ${String(context.name)}: ${e}`,
+        )
+      }
+    }
+  }
+
+  return catcher
+}
+
+class TestService {
+  @Catch
+  findSomeError() {
+    throw `Dangerous error ⚠️`
+  }
+
+  @Catch
+  randomErrors(): string | void {
+    if (Math.random() * 1e4 > 5e3) {
+      throw new RangeError(`Big number`)
+    } else {
+      return 'You are lucky. No errors happened'
+    }
+  }
+}
+
+const tester = new TestService()
+console.log(tester.findSomeError())
+console.log(tester.randomErrors())
